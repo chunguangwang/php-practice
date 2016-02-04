0146b4efbb87021f8bbcd0ec55e0ee899f9db38c
chunguangwang <chunguangwang1018@gmail.com>
2016-02-04 11:22:38 -0500

    Update visitor_tracking.php

--- php-counter-mysql/visitor_tracking.php
+++ php-counter-mysql/visitor_tracking.php
@@ -2,14 +2,18 @@
 require_once('visitors_connections.php');//the file with connection code and functions
 //get the required data
 
-$visitor_ip = GetHostByName("www.google.com");
+$visitor_ip = GetHostByName($_SERVER['REMOTE_ADDR']);
 $visitor_browser = getBrowserType();
 $visitor_hour = date("h");
 $visitor_minute = date("i");
 $visitor_day = date("d");
 $visitor_month = date("m");
 $visitor_year = date("Y");
-$visitor_refferer = GetHostByName("www.google.com");
+if(isset($_SERVER['HTTP_REFERER'])) {
+      $visitor_refferer = GetHostByName($_SERVER['HTTP_REFERER']);
+   }else{
+	  $visitor_refferer = "";
+   }
 $visited_page = selfURL();
 
 //write the required data to database
@@ -18,5 +22,5 @@ $sql = "INSERT INTO visitors_table (visitor_ip, visitor_browser, visitor_hour,
  visitor_minute, visitor_date, visitor_day, visitor_month, visitor_year, 
  visitor_refferer, visitor_page) VALUES ('$visitor_ip', '$visitor_browser', 
  '$visitor_hour', '$visitor_minute', CURRENT_TIMESTAMP, '$visitor_day', '$visitor_month', 
- '$visitor_year', '$visitor_refferer', 'GOOGLE')";
+ '$visitor_year', '$visitor_refferer', '$visited_page')";
 $result = mysql_query($sql) or trigger_error(mysql_error(),E_USER_ERROR);?>
