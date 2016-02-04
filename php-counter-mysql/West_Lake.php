3a77e1e6c9248538870fe0e40f1b7c11985062f7
chunguangwang <chunguangwang1018@gmail.com>
2016-02-04 11:25:14 -0500

    Create West_Lake.php

---
+++ php-counter-mysql/West_Lake.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<style>
+body {
+	background-image: url("hangzhou.jpg");
+}
+a {
+   text-decoration: none;
+   color: orange;
+   font-weight: 4em;
+   font-size: 3em;
+   }
+   a:hover {
+   text-decoration: underline;
+   font-weight: 6em;
+   font-size: 4em;
+   }
+</style>
+</head>
+<body>
+<a href="index.php">Back to index</a>
+<?php 
+include('visitor_tracking.php');?>
+</body>
+</html>
